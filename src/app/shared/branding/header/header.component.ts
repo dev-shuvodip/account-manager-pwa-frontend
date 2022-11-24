371314db6ffcd8f6c21af15d876c27328328013c
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-11-24 13:07:22 +0530

    minor change

--- src/app/shared/branding/header/header.component.ts
+++ src/app/shared/branding/header/header.component.ts
@@ -12,11 +12,11 @@ import { QuickLinksSidenavComponent } from './quick-links-sidenav/quick-links-si
 export class HeaderComponent {
 
   @ViewChild(QuickLinksSidenavComponent, { static: true }) quickLinksSidenav!: QuickLinksSidenavComponent;
+  pageTitle?: string = CommonConstants.ModulesRoutes.find(e => e.key == CommonConstants.Landing)?.displayText;
 
   constructor(private router: Router, private zone: NgZone) { }
 
   Navigate(e: any) {
-    debugger;
     this.zone.run(() => {
       this.router.navigate([e.currentTarget.dataset.link]);
     })
