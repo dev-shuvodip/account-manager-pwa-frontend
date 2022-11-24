d7f075b51c8e21b93ba4ea9458a52fcee79a51ea
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-11-25 00:07:46 +0530

    configured service worker updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,34 @@ export class AppComponent {
 
   ngOnInit() {
     if (this.swUpdate.isEnabled) {
-      this.swUpdate.checkForUpdate().then(() => {
-        if (confirm("You're using an old version of the control panel. Want to update?")) {
-          window.location.reload();
+      // this.swUpdate.checkForUpdate().then((updateAvailable) => {
+      //   if (updateAvailable) {
+      //     if (confirm("You're using an old version of the control panel. Want to update?")) {
+      //       window.location.reload();
+      //     }
+      //   }
+      // });
+      interval(600000).subscribe(() => {
+        console.log('UpdateService: Checking for Updates')
+        this.swUpdate.checkForUpdate();
+      });
+
+      this.swUpdate.versionUpdates.subscribe(async evt => {
+        console.log('UpdateService: versionUpdates', evt);
+        switch (evt.type) {
+          case 'VERSION_DETECTED':
+            console.log(`Downloading new app version: ${evt.version.hash}`);
+            break;
+          case 'VERSION_READY':
+            console.log(`Current app version: ${evt.currentVersion.hash}`);
+            console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+            if (confirm("You're using an old version of the control panel. Want to update?")) {
+              window.location.reload();
+            }
+            break;
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+            break;
         }
       });
     }
