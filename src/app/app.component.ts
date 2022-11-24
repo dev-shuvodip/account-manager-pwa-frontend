d274c1004068f6b11f6c9b9a13339d57c7970260
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-11-24 23:25:42 +0530

    config for pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'account-manager-client';
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate().then(() => {
+        if (confirm("You're using an old version of the control panel. Want to update?")) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
