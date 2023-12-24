129ecfaa8e2c0ef03a97e280e5ae5a18a4cf1446
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2023-12-24 17:49:44 +0530

    minor changes

--- src/account-manager/account-manager.module.ts
+++ src/account-manager/account-manager.module.ts
@@ -19,7 +19,6 @@ import { SharedModule } from './shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { AuthService } from './shared/auth/services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FirebaseSettings } from 'firebase.config';
 
